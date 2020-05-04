82468f37ba661d88a071d36f8c7ac46f3547daf5
JoostBee <j.burggraaff@student.fontys.nl>
2020-05-04 13:22:05 +0200

    Added logic, data layer and a interface

---
+++ Data/Interfaces/IDataProduct.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Model.DataTransferObject;
+
+namespace Data.Interfaces
+{
+    public class IDataProduct
+    {
+        List<ProductDTO> void GetProductList();
+    }
+}
---
+++ GCControllerShopModels/DataTransferObjects/ProductDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.DataTransferObjects
+{
+    class ProductDTO
+    {
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+    }
+}
